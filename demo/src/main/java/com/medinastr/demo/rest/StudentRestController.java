2c6554e69fae9cae92ab9ec3601a1feff36eb76d
medinastr <pedromstracieri@gmail.com>
2024-07-25 18:17:26 -0300

    exception created for non-existent id

--- demo/src/main/java/com/medinastr/demo/rest/StudentRestController.java
+++ demo/src/main/java/com/medinastr/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.medinastr.demo.rest;
 
 import com.medinastr.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +33,25 @@ public class StudentRestController {
     // define endpoint or "/students/{studentId}" - return student at index
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        // check student ID
+        if(studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student ID not found.");
+        }
         return students.get(studentId);
+    }
+
+    // add exception handler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
