407ab6c4b2692deda82c1ce253e4ed02b276b348
medinastr <pedromstracieri@gmail.com>
2024-09-04 14:42:59 -0300

    spring security to add authorities to access endpoints

---
+++ crudapp/src/main/java/com/medinastr/spring/crudapp/security/DemoSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.medinastr.spring.crudapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    // add support for JDBC
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) { // it will inject data source
+        // tell spring security to use JDBC authentication with our data source
+        return new JdbcUserDetailsManager(dataSource); // all information is stored in database
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                configurer -> configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use http basic authorization
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        // disable CSRF
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+}
